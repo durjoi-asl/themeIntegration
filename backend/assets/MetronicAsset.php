455bdc9da9b3bd281ca86e0338e21cd7528bd83c
mehedi-durjoi <m.h.durjoi@gmail.com>
2020-08-16 16:06:34 +0600

    Dashboard basic theme integrated

---
+++ backend/assets/MetronicAsset.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class MetronicAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'metronic/plugins/custom/fullcalendar/fullcalendar.bundle.css',
+        'metronic/plugins/custom/prismjs/prismjs.bundle.css',
+        'metronic/css/style.bundle.css',
+        'metronic/css/themes/layout/header/base/light.css',
+        'metronic/css/themes/layout/header/menu/light.css',
+        'metronic/css/themes/layout/brand/dark.css',
+        'metronic/css/themes/layout/aside/dark.css',
+    ];
+    public $js = [
+      'metronic/plugins/global/plugins.bundle.js',
+      'metronic/plugins/custom/prismjs/prismjs.bundle.js',
+      'metronic/js/scripts.bundle.js',
+      'metronic/plugins/custom/fullcalendar/fullcalendar.bundle.js',
+      'metronic/js/pages/widgets.js',
+    ];
+    public $depends = [
+        // 'yii\web\YiiAsset',
+        // 'yii\bootstrap4\BootstrapAsset',
+    ];
+}
