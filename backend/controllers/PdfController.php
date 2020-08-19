9ff99fe5d1e370ef7a0eb9ab8a691b5fb6397709
mehedi-durjoi <m.h.durjoi@gmail.com>
2020-08-19 12:59:48 +0600

    Pdf Protection added

--- backend/controllers/PdfController.php
+++ backend/controllers/PdfController.php
@@ -8,20 +8,6 @@ use Yii;
 class PdfController extends \yii\web\Controller
 {
     public function actionCreatePdf() {
-      // $pdf = new Pdf([
-      //   'mode'        => Pdf::MODE_CORE,
-      //   'format'      => Pdf::FORMAT_A4,
-      //   'orientation' => Pdf::ORIENT_PORTRAIT,
-      //   'destination' => Pdf::DEST_BROWSER,
-      //   'content'     => "This is first PDF",
-      //   'options' => ['title' => 'First PDF Title'],
-      //    // call mPDF methods on the fly
-      //   'methods' => [
-      //       'SetHeader'=>['PDF Header'],
-      //       'SetFooter'=>['{PAGENO}'],
-      //   ]
-      // ]);
-
       $content = $this->renderPartial('_pdf');
 
       $pdf = Yii::$app->pdf;
@@ -33,12 +19,8 @@ class PdfController extends \yii\web\Controller
       $pdf->marginFooter = 10;
 
       $mpdf = $pdf->api;
-      // $pdf->content = "This is first Global Pdf";
-      // $pdf->options = ['title' => 'First PDF Title'];
-      // $pdf->methods = ['SetHeader'=>['PDF Header'],
-      //       'SetFooter'=>['{PAGENO}'],];
 
-      $mpdf->SetProtection(array('print'));
+      $mpdf->SetProtection(['print'], '123456', 'abcd' );
       $mpdf->SetTitle("Acme Trading Co. - Invoice");
       $mpdf->SetAuthor("Acme Trading Co.");
       $mpdf->SetWatermarkText("Paid");
@@ -46,11 +28,7 @@ class PdfController extends \yii\web\Controller
       $mpdf->watermark_font = 'DejaVuSansCondensed';
       $mpdf->watermarkTextAlpha = 0.1;
       $mpdf->SetDisplayMode('fullpage');
-
-      // $mpdf->SetHeader("Pdf Header");
-      // $pdf->SetHeader = "Pdf Header";
       $mpdf->WriteHtml($content);
-      // $mpdf->SetFooter('{PAGENO}');
 
 
       return $mpdf->output();
