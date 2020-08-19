ccb1a70e18780a7b203ef8d11b53ab897f3d5e3e
mehedi-durjoi <m.h.durjoi@gmail.com>
2020-08-19 11:48:01 +0600

    Test Pdf generation done

--- backend/controllers/PdfController.php
+++ backend/controllers/PdfController.php
@@ -2,6 +2,25 @@
 
 namespace backend\controllers;
 
+use kartik\mpdf\Pdf;
+
 class PdfController extends \yii\web\Controller
 {
+    public function actionCreatePdf() {
+      $pdf = new Pdf([
+        'mode'        => Pdf::MODE_CORE,
+        'format'      => Pdf::FORMAT_A4,
+        'orientation' => Pdf::ORIENT_PORTRAIT,
+        'destination' => Pdf::DEST_BROWSER,
+        'content'     => "This is first PDF",
+        'options' => ['title' => 'First PDF Title'],
+         // call mPDF methods on the fly
+        'methods' => [
+            'SetHeader'=>['PDF Header'],
+            'SetFooter'=>['{PAGENO}'],
+        ]
+      ]);
+
+      return $pdf->render();
+    }
 }
